af7438e1b81653a4517868e830ff2bfca62fd314
jayachandra <jayachandraoggy@gmail.com>
2018-02-02 20:14:04 +0530

    gp and gg values initialization

---
+++ func1.js
@@ -0,0 +1,13 @@
+function addnew()
+{
+	//array initialization
+	var gp=document.getElementById("gp");
+	var gg=document.getElementById("gg");
+	//var result=str.value();
+	alert("entered usn("+gp.value+") is in the correct form.\n");
+	alert("entered usn("+gg.value+") is in the correct form.\n");
+}
+function finish()
+{
+
+}
