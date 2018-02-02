e7ad34564ee6a8409e1146b72cab3fb18db2dc35
jayachandra <jayachandraoggy@gmail.com>
2018-02-02 20:55:38 +0530

    array initialization condition(2 if stats)

--- func1.js
+++ func1.js
@@ -1,11 +1,22 @@
-function addnew()
-{
+var t=0;
+function addnew(){
 	//array initialization
 	var gp=document.getElementById("gp");
 	var gg=document.getElementById("gg");
+	
 	//var result=str.value();
-	alert("entered usn("+gp.value+") is in the correct form.\n");
-	alert("entered usn("+gg.value+") is in the correct form.\n");
+	
+	if(t == 0){
+		var gpa= new Array(gp);
+		alert(t);
+		t++;
+		return;
+	}
+	if(t != 0){
+		alert(t);
+		gpa[t++]=gp;
+		alert(t);
+	}
 }
 function finish()
 {
